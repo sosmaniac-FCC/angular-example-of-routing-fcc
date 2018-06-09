c55a655b999711b93b923e2b3088145d9ea01958
John Simmons <jgsimmons42@students.tntech.edu>
2018-06-08 20:50:09 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AComponent } from './components/a/a.component';
+import { BComponent } from './components/b/b.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AComponent,
+    BComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
