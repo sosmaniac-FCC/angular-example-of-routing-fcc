3f2cb19d3109cca89c3e83ec4a129e70f0f2dbbf
John Simmons <jgsimmons42@students.tntech.edu>
2018-06-08 22:37:32 -0500

    example 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './modules/app-routing/app-routing.module';
+
 import { AppComponent } from './app.component';
 import { AComponent } from './components/a/a.component';
 import { BComponent } from './components/b/b.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AComponent,
-    BComponent
+    BComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    AppRoutingModule, // routing feature module
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AComponent } from '../../components/a/a.component';
+import { BComponent } from '../../components/b/b.component';
+import { PageNotFoundComponent } from '../../components/page-not-found/page-not-found.component';
+
+// all of our soon-to-be routes!
+const routes: Routes = [
+  {
+    path: 'A',
+    component: AComponent
+  },
+  {
+    path: 'B',
+    component: BComponent
+  },
+  {
+    path: '',
+    redirectTo: 'A',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot()
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
